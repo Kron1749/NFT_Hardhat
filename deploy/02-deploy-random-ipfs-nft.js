ddd9d15551c13e5fdcfbf072bbade8862bcd8b70
Kron1749 <drakgr98@gmail.com>
2022-09-04 12:57:59 +0300

    Updated helper-hardhat-config Created mocks Created deploy script for random nft

---
+++ deploy/02-deploy-random-ipfs-nft.js
@@ -0,0 +1,42 @@
+const { getNamedAccounts, deployments, network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let tokenUris
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        tokenUris = await handleTokenUris()
+    }
+
+    let vrfCoordinatorV2Address, subscriptionId
+
+    if (developmentChains.includes(network.name)) {
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address
+        const tx = await VRFCoordinatorV2Mock.createSubscription()
+        const txReceipt = await tx.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+
+    log("------------------------------------------")
+    const args = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId].gasLane,
+        networkConfig[chainId].callbackGasLimit,
+        // token uri
+        networkConfig[chainId].mintFee,
+    ]
+}
+
+async function handleTokenUris() {
+    tokenUris = []
+
+    return tokenUris
+}
