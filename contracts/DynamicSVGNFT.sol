3aa481b0ca5e9ea888bc1560fc07a5d2b975d3ee
Kron1749 <drakgr98@gmail.com>
2022-09-05 17:53:03 +0300

    Created tests for DynamicSVGNFT

--- contracts/DynamicSVGNFT.sol
+++ contracts/DynamicSVGNFT.sol
@@ -7,8 +7,8 @@ import "base64-sol/base64.sol";
 
 contract DynamicSVGNFT is ERC721 {
     uint256 private s_tokenCounter;
-    string private i_lowImageURI;
-    string private i_highImageURI;
+    string private s_lowImageURI;
+    string private s_highImageURI;
     string private constant BASE64_ENCODED_SVG_PREFIX = "data:image/svg+xml;base64,";
     AggregatorV3Interface internal immutable i_priceFeed;
     mapping(uint256 => int256) public s_tokenIdToHighValue;
@@ -21,8 +21,8 @@ contract DynamicSVGNFT is ERC721 {
         string memory highSvg
     ) ERC721("Dynamic SVG NFT", "DSN") {
         s_tokenCounter = 0;
-        i_lowImageURI = svgToImageURI(lowSvg);
-        i_highImageURI = svgToImageURI(highSvg);
+        s_lowImageURI = svgToImageURI(lowSvg);
+        s_highImageURI = svgToImageURI(highSvg);
         i_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
@@ -33,8 +33,8 @@ contract DynamicSVGNFT is ERC721 {
 
     function mintNft(int256 highValue) public {
         s_tokenIdToHighValue[s_tokenCounter] = highValue;
-        s_tokenCounter += 1;
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
         emit CreatedNFT(s_tokenCounter, highValue);
     }
 
@@ -46,10 +46,10 @@ contract DynamicSVGNFT is ERC721 {
         require(_exists(tokenId), "URI Query for nonexistent token");
         // string memory imageURI = "hi!";
         (, int256 price, , , ) = i_priceFeed.latestRoundData();
-        string memory imageURI = i_lowImageURI;
+        string memory imageURI = s_lowImageURI;
 
         if (price >= s_tokenIdToHighValue[tokenId]) {
-            imageURI = i_highImageURI;
+            imageURI = s_highImageURI;
         }
         return
             string(
@@ -69,5 +69,21 @@ contract DynamicSVGNFT is ERC721 {
                     )
                 )
             );
+    }
+
+    function getLowSVG() public view returns (string memory) {
+        return s_lowImageURI;
+    }
+
+    function getHighSVG() public view returns (string memory) {
+        return s_highImageURI;
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return i_priceFeed;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
     }
 }
